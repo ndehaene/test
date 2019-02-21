7b98f63478caf8bc63d2a7e0ea905f97d73e4201
Noémie Dehaene <noemie.dehaene@wanadoo.fr>
2019-02-21 10:54:03 +0100

    first commit

---
+++ src/main/java/com/futurskill/formation/App.java
@@ -0,0 +1,20 @@
+package com.futurskill.formation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static void(){
+		System.out.println("Modif immédiate");
+	}
+    public static void modif1(){
+		System.out.println("Modif1");
+	}
+}
